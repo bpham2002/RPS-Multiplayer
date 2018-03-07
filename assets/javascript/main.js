1ec0b16eb4ba539a2aa2226fbe7b8740db691206
unknown <bpham2002@gmail.com>
2018-03-07 14:50:33 -0800

    commpleted connect and disconect

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -17,39 +17,94 @@
  var database = firebase.database();
 
  // Initializing our click count at 0
- var isplayer_1 = false;
- var isplayer_2 = false;
- var player = 0;
 
+ var player = 0;
+ var totalplayers = 0;
 
  $("#list-1").hide()
  $("#list-2").hide()
+ var num = sessionStorage.getItem('player')
+ database.ref('players/' + num).remove();
+ database.ref().once('value').then(function(snapshot) {
+     totalplayers = snapshot.val().members.mem;
+
+     if (num > 0) {
+         totalplayers--;
+         database.ref('members/').set({
+             mem: totalplayers
+         })
+         localStorage.setItem('isplayer_' + num, false);
+         sessionStorage.setItem('player', player)
+     } else {
+         localStorage.setItem('isplayer_1', false);
+         localStorage.setItem('isplayer_2', false);
+         sessionStorage.setItem('player', player)
+     }
+ })
+
+
+
+
+
  database.ref().on('value', function(shot) {
      if (shot.val() === null) {
          database.ref('members/').set({
              mem: 0
          })
 
-     } else if (shot.val().members.mem > 0) {
+     } else { //if (shot.val().members.mem > 0) {
+         var temp = shot.val().members.mem;
+         for (var i = 1; i <= temp; i++) {
+             database.ref('players/' + i + '/name').once('value', function(snapshot) {
 
+                 $("#player-" + i).text(snapshot.val())
+             });
+             database.ref('players/' + i + '/wins').once('value', function(snapshot) {
 
+                 $("#player-wins-" + i).text(snapshot.val())
+             });
+             database.ref('players/' + i + '/loses').once('value', function(snapshot) {
+
+                 $("#player-loses-" + i).text(snapshot.val())
+
+             })
+         }
      }
  })
 
  // Functions
-
+ function setPlayer(num) {
+     num++;
+     database.ref('members/').set({
+         mem: num
+     });
+     database.ref('players/' + num).set({
+         name: $("#add-user").val().trim(),
+         wins: 0,
+         loses: 0
+     });
+     $("#player").text('You are player-' + num);
+     $("#register").empty()
+     $("#list-" + num).show()
+     sessionStorage.setItem('player', num)
+ }
  // ================================================================================
 
  // On Click
  $("#add-button").on("click", function() {
      event.preventDefault();
+     var isplayer_1 = localStorage.getItem('isplayer_1');
+     var isplayer_2 = localStorage.getItem('isplayer_2');
      return database.ref('members/').once('value').then(function(snapshot) {
-         var member = snapshot.val().mem
-         if (member < 2) {
-             member++;
-             database.ref('members/').set({
-                 mem: member
-             });
+
+         var member = parseInt(snapshot.val().mem)
+         if (member < 1) {
+             setPlayer(member);
+             localStorage.setItem('isplayer_1', true)
+         } else if (member < 2 && (isplayer_1 === 'true')) {
+             setPlayer(member);
+             localStorage.setItem('isplayer_2', true)
+         } else if (member < 2 && (isplayer_1 === 'false')) {
              database.ref('players/' + member).set({
                  name: $("#add-user").val().trim(),
                  wins: 0,
@@ -58,13 +113,14 @@
              $("#player").text('You are player-' + member);
              $("#register").empty()
              $("#list-" + member).show()
-             player = member;
-             sessionStorage.setItem('player', player)
-             database.ref('players/' + member).on('value', function(snapshot) {
-                 $("#player-" + member).text(snapshot.val().name)
-             })
+             sessionStorage.setItem('player', member)
+             localStorage.setItem('isplayer_1', true)
+             member++;
+             database.ref('members/').set({
+                 mem: member
+             });
+
          }
-
-
      })
+
  });
